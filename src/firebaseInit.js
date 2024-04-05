3003470beced0d3b5683261c38fc7d46f1339d05
Rahul Nain <rahulnain@Rahuls-MacBook-Air.local>
2024-04-06 03:28:03 +0530

    2 more warnings removed

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,12 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBLdHuEe0zJOnoE29KXoY7V2FD7NGBe06g",
   authDomain: "emart-19196.firebaseapp.com",
@@ -19,6 +12,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db=getFirestore(app);
 
