cf94c3e0e07c80de63889ee570c6f73d2ddf58e9
Rahul Nain <rahulnain@Rahuls-MacBook-Air.local>
2024-04-11 20:58:16 +0530

    handled infinite loading if not logged in and pressed add to cart button

--- src/components/Items/product.js
+++ src/components/Items/product.js
@@ -4,10 +4,12 @@ import { useParams } from "react-router-dom";
 import { useContext,useEffect } from "react";
 import { UserContext } from "../../contexts/usercontext";
 import { Carousel } from "./Carousel";
+import { useNavigate } from "react-router-dom";
 import Itemstyle from "./Itemstyle.module.css"
 
 export const Product = () => {
-    const handleAddtoCart = useContext(UserContext).handleAddtoCart
+    const {User,handleAddtoCart} = useContext(UserContext);
+    const navigate=useNavigate();
     let { name } = useParams();
     let ind = store.findIndex((item)=>item.Name===name);
     const obj = store[ind];
@@ -17,6 +19,7 @@ export const Product = () => {
       useEffect(() => {
         window.scrollTo(0, 0)
       })
+    
     return (<>
         <div class="container my-5" >
             <div class="row">
@@ -29,7 +32,7 @@ export const Product = () => {
                     <div className={productstyle.Productinfo}>
                         <div className={productstyle.details}>
                             <p>{obj.Details}</p>
-                            <p style={{fontSize:"1.7rem"}}>$&nbsp; {obj.Price} &nbsp;<button type="button" class="btn btn-warning" style={{width:"20rem",fontSize:"1.5rem"}} onClick={()=>handleAddtoCart(obj)}><b>ADD TO CART</b></button></p>
+                            <p style={{fontSize:"1.7rem"}}>$&nbsp; {obj.Price} &nbsp;<button type="button" class="btn btn-warning" style={{width:"20rem",fontSize:"1.5rem"}} onClick={()=>{(User)?handleAddtoCart(obj):navigate("/auth/signin")}}><b>ADD TO CART</b></button></p>
                         </div>
                         <div className={productstyle.specdiv}>
                             <b style={{ fontSize: "1.7rem" }}>SPECIFICATIONS:</b>
